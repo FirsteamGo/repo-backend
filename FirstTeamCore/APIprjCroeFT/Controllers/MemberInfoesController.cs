354a1883559df0c3c1b6e01012f9783cb43a8222
zhiyuan <skunk00605@yahoo.com.tw>
2023-03-15 20:16:37 +0800

    會員中心api更新

--- FirstTeamCore/APIprjCroeFT/Controllers/MemberInfoesController.cs
+++ FirstTeamCore/APIprjCroeFT/Controllers/MemberInfoesController.cs
@@ -85,11 +85,11 @@ namespace APIprjCroeFT.Controllers
         // POST: api/MemberInfoes
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<string> PostMemberInfo(MemberInfoDTO AA)
+        public async Task<string> PostMemberInfo(MemberInfoDTO MI)
         {
-            MemberInfo user = (_context.MemberInfo.FirstOrDefault(t => t.會員帳號.Equals(AA.account) && t.會員密碼.Equals(AA.password)));
+            MemberInfo user = (_context.MemberInfo.FirstOrDefault(t => t.會員帳號.Equals(MI.account) && t.會員密碼.Equals(MI.password)));
 
-            if (user != null && user.會員密碼.Equals(AA.password))
+            if (user != null && user.會員密碼.Equals(MI.password))
             {
                 var result = new
                 {
@@ -124,17 +124,17 @@ namespace APIprjCroeFT.Controllers
 
         // 註冊用 post
         [HttpPost("SignUp")]
-        public async Task<string> PostMemberInfo(SignupDTO AA)
+        public async Task<string> PostMemberInfo(SignupDTO MI)
         {
             MemberInfo mem = new MemberInfo
             {
-                性別 = AA.性別,
-                姓名 = AA.姓名,
-                會員帳號 = AA.會員帳號,
-                會員密碼 = AA.會員密碼,
-                電話號碼 = AA.電話號碼,
-                出生日期 = AA.出生日期,
-                連絡信箱 = AA.連絡信箱,
+                性別 = MI.性別,
+                姓名 = MI.姓名,
+                會員帳號 = MI.會員帳號,
+                會員密碼 = MI.會員密碼,
+                電話號碼 = MI.電話號碼,
+                出生日期 = MI.出生日期,
+                連絡信箱 = MI.連絡信箱,
 
             };
 
@@ -144,6 +144,32 @@ namespace APIprjCroeFT.Controllers
             return "註冊成功";
         }
 
+
+        // 修改用 Put
+        [HttpPut("Revise/{會員ID}")]
+        public async Task<string> PutMemberInfo(int 會員ID, ReviseDTO MI)
+        {
+            // 添加'Access-Control-Allow-Origin'標頭
+            HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            
+            var memberInfo = _context.MemberInfo.Where(m => m.會員id == 會員ID).First();
+
+            //memberInfo.會員帳號 = MI.會員帳號;
+            //memberInfo.會員密碼 = MI.會員密碼;
+            MemberInfo mem = new MemberInfo
+            {
+
+                姓名 = MI.姓名,
+                會員密碼 = MI.會員密碼,
+                電話號碼 = MI.電話號碼,
+                連絡信箱 = MI.連絡信箱,
+
+            };
+
+            await _context.SaveChangesAsync();
+
+            return "修改成功";
+        }
 
 
 
