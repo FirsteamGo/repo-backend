354afb23298baf54464fbec0e43ea1ddf490574a
zhiyuan <skunk00605@yahoo.com.tw>
2023-03-25 10:31:00 +0800

    會員更新

--- FirstTeamCore/APIprjCroeFT/Controllers/MemberInfoesController.cs
+++ FirstTeamCore/APIprjCroeFT/Controllers/MemberInfoesController.cs
@@ -9,6 +9,8 @@ using APIprjCroeFT.Models;
 using Microsoft.AspNetCore.Cors;
 using System.Text.Json;
 using APIprjCroeFT.DTO;
+using System.Net.Mail;
+using System.Net;
 
 namespace APIprjCroeFT.Controllers
 {
@@ -128,6 +130,7 @@ namespace APIprjCroeFT.Controllers
         {
             MemberInfo mem = new MemberInfo
             {
+                照片= MI.照片,
                 性別 = MI.性別,
                 姓名 = MI.姓名,
                 會員帳號 = MI.會員帳號,
@@ -143,6 +146,62 @@ namespace APIprjCroeFT.Controllers
 
             return "註冊成功";
         }
+
+        // 上傳圖片用 post
+        [HttpPost("api/upload")]
+        public async Task<IActionResult> Upload()
+        {
+            var file = Request.Form.Files[0];
+            var fileName = file.FileName;
+            var filePath = Path.Combine("C:/Users/zhi yuan/Desktop/期末專案/0_期末專案/repo-backend/FirstTeamCore/FirstTeamCore/wwwroot/images", fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return Ok(new { filePath });
+        }
+
+
+        //// 發送驗證信用 post (有問題)
+        //[HttpPost("api/send-email")]
+        //public async Task<IActionResult> SendEmail([FromBody] EmailData emailData)
+        //{
+        //    try
+        //    {
+        //        var email = emailData.連絡信箱;
+        //        var message = "驗證測試";
+
+        //        // create reusable transporter object using the default SMTP transport
+        //        var transporter = new SmtpClient("smtp.gmail.com")
+        //        {
+        //            Port = 587,
+        //            Credentials = new NetworkCredential("your-email@gmail.com", "your-email-password"),
+        //            EnableSsl = true
+        //        };
+
+        //        // create message
+        //        var mailMessage = new MailMessage();
+        //        mailMessage.From = new MailAddress("your-email@gmail.com");
+        //        mailMessage.To.Add(new MailAddress(email));
+        //        mailMessage.Subject = "Email Verification";
+        //        mailMessage.Body = $"Please verify your email by clicking on the following link: {message}";
+
+        //        // send mail
+        //        await transporter.SendMailAsync(mailMessage);
+
+        //        return Ok("Email Sent");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        //    }
+        //}
+
+
+
+
 
 
         // 修改用 Put
---
+++ FirstTeamCore/APIprjCroeFT/DTO/EmailDataDTO.cs
@@ -0,0 +1,16 @@
+﻿//namespace APIprjCroeFT.DTO
+//{
+
+//    public class EmailData
+//    {
+
+
+
+//        public string? 連絡信箱 { get; set; }
+//    }
+//}
+
+ 
+
+
+
